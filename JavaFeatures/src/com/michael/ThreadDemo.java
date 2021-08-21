5341423fc9ae45c83af0f6a667dab664e24ee140
Michael Garza <michaelgarza496@gmail.com>
2021-08-21 11:01:28 -0400

    Learning more about threads.

--- JavaFeatures/src/com/michael/ThreadDemo.java
+++ JavaFeatures/src/com/michael/ThreadDemo.java
@@ -10,23 +10,42 @@ package com.michael;
 
 public class ThreadDemo {
 
+	public static void main(String[] args) throws InterruptedException {
+		ExtendedThread extendedThread = new ExtendedThread();
+		extendedThread.start();
+		System.out.println(extendedThread.isAlive());
 
-	
+		extendedThread.join();
+		System.out.println(extendedThread.isAlive());
+		Thread.sleep(3000);
+
+		new Thread(new ImplementRunnable()).start();
+		System.out.println("Main method thread ends");
+	}
+
 }
 
 /*
-	Extending way
-	1. Extend Thread class
-	2. Override run() method to define operations for thread
-	3. Create instance/s of thread and invoke start() method. The start method
-	   begins the thread's execution where the JVM invokes the run() method. 
-*/
+ * Extending way 1. Extend Thread class 2. Override run() method to define
+ * operations for thread 3. Create instance/s of thread and invoke start()
+ * method. The start method begins the thread's execution where the JVM invokes
+ * the run() method.
+ */
 class ExtendedThread extends Thread {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		super.run();
+		System.out.println("Extended run()");
 	}
-	
+
+}
+
+class ImplementRunnable implements Runnable {
+
+	@Override
+	public void run() {
+		System.out.println("Implemented run()");
+
+	}
+
 }

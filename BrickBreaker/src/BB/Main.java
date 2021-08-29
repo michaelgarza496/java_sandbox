29c2c1ceb7ed1164a13270953321dbae213a8b0f
Michael Garza <michaelgarza496@gmail.com>
2021-08-28 23:11:28 -0400

    Adding System.setProperty field to allow smoother game play in linux.

--- BrickBreaker/src/BB/Main.java
+++ BrickBreaker/src/BB/Main.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 public class Main {
 
 	public static void main(String[] args) {
+		System.setProperty("sun.java2d.opengl", "true");
 
 		JFrame f = new JFrame("Brick Breaker");
 		GamePlay gamePlay = new GamePlay();
